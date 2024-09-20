02d691384ac604d09ef04a223383f383847980eb
manish5476 <msms5476@gmail.com>
2024-09-21 00:17:41 +0530

    manish

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,23 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductMasterComponent } from './component/product-master/product-master.component';
 import { UserMasterComponent } from './component/user-master/user-master.component';
-
 import { FormsModule } from '@angular/forms'; // Import FormsModule
+import { InputTextModule } from 'primeng/inputtext';
+import { FloatLabelModule } from 'primeng/floatlabel';
+import { ButtonModule } from 'primeng/button';
+import { NavbarComponent } from './component/navbar/navbar.component';
 
 @NgModule({
-  declarations: [AppComponent, ProductMasterComponent, UserMasterComponent],
-  imports: [FormsModule, BrowserModule, AppRoutingModule, HttpClientModule],
+  declarations: [AppComponent, ProductMasterComponent, UserMasterComponent, NavbarComponent],
+  imports: [
+    FormsModule,
+    ButtonModule,
+    FloatLabelModule,
+    InputTextModule,
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+  ],
   providers: [provideClientHydration()],
   bootstrap: [AppComponent],
 })
